ffe1e99cfdc830c5e189fe955b0b51f8e0442154
Theerdha Raju <theerdharaju4@gmail.com>
2026-05-06 22:44:05 +0530

    Hide password fields and improve login/signup UI

--- school_app/views.py
+++ school_app/views.py
@@ -3,7 +3,7 @@ from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import login
 from django.contrib.auth.decorators import login_required
 from .models import Student, Teacher, Course
-from .forms import StudentForm, TeacherForm, CourseForm
+from .forms import StudentForm, TeacherForm, CourseForm, SignUpForm
 
 @login_required
 def index(request):
@@ -124,12 +124,12 @@ def course_delete(request, pk):
 
 def signup(request):
     if request.method == 'POST':
-        form = UserCreationForm(request.POST)
+        form = SignUpForm(request.POST)
         if form.is_valid():
             user = form.save()
             login(request, user)
             return redirect('index')
     else:
-        form = UserCreationForm()
+        form = SignUpForm()
     return render(request, 'registration/signup.html', {'form': form})
 
