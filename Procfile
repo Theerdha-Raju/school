c6eba54f3ed8409d525f4497155767475cd18dca
Theerdha Raju <theerdharaju4@gmail.com>
2026-05-06 22:12:26 +0530

    Add requirements.txt and deployment configs

---
+++ Procfile
@@ -0,0 +1,2 @@
+# Command to run the web server in production (Gunicorn)
+web: gunicorn school_project.wsgi:application
--- school_project/settings.py
+++ school_project/settings.py
@@ -21,6 +21,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware', # Essential for serving static files on AWS
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -77,6 +78,8 @@ USE_I18N = True
 USE_TZ = True
 
 STATIC_URL = 'static/'
+# STATIC_ROOT is where 'collectstatic' will put all CSS/JS files for production
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 LOGIN_REDIRECT_URL = 'index'
